49704741af1ddce3a78c319eff11ac972e150774
Jake Adams <jadams.html@gmail.com>
2019-03-02 11:58:15 +0000

    correctly using Places API & finding users location using promise

--- js/app.js
+++ js/app.js
@@ -1,70 +1,72 @@
-console.log('JS loaded');
-
-
-      // This example requires the Places library. Include the libraries=places
-      // parameter when you first load the API. For example:
-      // <script src="https://maps.googleapis.com/maps/api/js?key=YOUR_API_KEY&libraries=places">
-
-      var map;
-
-      function initMap() {
-        // Create the map.
-        var pyrmont = {lat: -33.866, lng: 151.196};
-        map = new google.maps.Map(document.getElementById('map'), {
-          center: pyrmont,
-          zoom: 17
-        });
-
-        // Create the places service.
-        var service = new google.maps.places.PlacesService(map);
-        var getNextPage = null;
-        var moreButton = document.getElementById('more');
-        moreButton.onclick = function() {
-          moreButton.disabled = true;
-          if (getNextPage) getNextPage();
-        };
-
-        // Perform a nearby search.
-        service.nearbySearch(
-            {location: pyrmont, radius: 500, type: ['store']},
-            function(results, status, pagination) {
-              if (status !== 'OK') return;
-
-              createMarkers(results);
-              moreButton.disabled = !pagination.hasNextPage;
-              getNextPage = pagination.hasNextPage && function() {
-                pagination.nextPage();
-              };
-            });
+//http request returning in JSON format
+var getJSON = function(url, callback) {
+    var xhr = new XMLHttpRequest();
+    xhr.open('GET', url, true);
+    xhr.responseType = 'json';
+    xhr.onload = function() {
+      var status = xhr.status;
+      if (status === 200) {
+        callback(null, xhr.response);
+      } else {
+        callback(status, xhr.response);
       }
+    };
+    xhr.send();
+};
 
-      function createMarkers(places) {
-        var bounds = new google.maps.LatLngBounds();
-        var placesList = document.getElementById('places');
+//format url correctly for users location, fallback location, API KEY, parameters etc.
+function formatUrl(){
+  let userLatLng;
+  getLocation()
+   .then((position) => {
+     userLatLng = position.coords;
+     console.log(userLatLng)
+   })
+   .catch((err) => {
+     console.error(err.message);
+     userLatLng = false;
+   });
 
-        for (var i = 0, place; place = places[i]; i++) {
-          var image = {
-            url: place.icon,
-            size: new google.maps.Size(71, 71),
-            origin: new google.maps.Point(0, 0),
-            anchor: new google.maps.Point(17, 34),
-            scaledSize: new google.maps.Size(25, 25)
-          };
+   function generateUrl(){
 
-          var marker = new google.maps.Marker({
-            map: map,
-            icon: image,
-            title: place.name,
-            position: place.geometry.location
-          });
+   }
+}
 
-          console.log(place)
 
-          var li = document.createElement('li');
-          li.textContent = place.name;
-          placesList.appendChild(li);
+//getting JSON and dealing with error
+// getJSON('https://cors-anywhere.herokuapp.com/https://maps.googleapis.com/maps/api/place/details/json?placeid=ChIJuVnRajzU3ogRpdz30sTIqkc&key=AIzaSyD137cSJRa1LV_8djUKFlUeGgHd9MNh4FU',
+// function(err, data) {
+//   if (err !== null) {
+//     console.log('Something went wrong: ' + err);
+//   } else {
+//     let place = data.result
+//   }
+// });
 
-          bounds.extend(place.geometry.location);
-        }
-        map.fitBounds(bounds);
-      }
+//get users lat and lng
+function getLocation() {
+
+  var options = {
+    enableHighAccuracy: true,
+    maximumAge: 0
+  };
+
+  function success(pos) {
+    var crd = pos.coords;
+
+    console.log('Your current position is:');
+    console.log(`Latitude : ${crd.latitude}`);
+    console.log(`Longitude: ${crd.longitude}`);
+  }
+
+  function error(err) {
+    console.warn(`ERROR(${err.code}): ${err.message}`);
+  }
+
+  return new Promise(function (success, error) {
+    navigator.geolocation.getCurrentPosition(success, error, options);
+  });
+}
+
+
+formatUrl();
